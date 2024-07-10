1386bff7b454a33db68c8479b0f8d0e5af41f606
Benjamin Schoolland <bschoolland@gmail.com>
2024-07-09 19:14:41 -0700

    explain how to create the API routes

--- src/app/page.js
+++ src/app/page.js
@@ -21,8 +21,22 @@ export default function Home() {
   })
   return (
     <div>
-      <h1>The below data is loaded from an API:</h1>
-      <p>{myData}</p>
+      <h1>How to use an API in Next.js</h1>
+      <h2>Step 1: set up next.js</h2>
+      <p>create a next.js project with <code>npx create-next-app</code></p>
+      <h2>Step 2: Create a File for your api Route</h2>
+      <p>Look for the <code>/api</code> or <code>/src/api</code> folder in your project</p>
+      <p>create a new folder within that folder.  Name it whatever you'd like the api route to be called. My example is called <code>example_api</code></p>
+      <p>create a new file within that folder.  Name it <code>route.js</code> this name is mandatory just like "page.js" is mandatory for pages</p>
+      <h2>Step 3: Write your API route</h2>
+      <p>The thing that confused me most was the fact that you HAVE to name your functions after the HTTP method they handle</p>
+      <p>Take a look at my api route at <code>src/api/example_api/route.js</code> to learn how to write a basic API route</p>
+      <h2>Step 4: Use the API in your page</h2>
+      <p>Once you create the API route, you just have to send a request to it from your page.  I'm using fetch, but you can use axios or whatever you'd like</p>
+
+      <h2>Step 5: Display the data</h2>
+      <p>Display or use the data in some way! The following text is filled out by the data we got from the server:</p>
+      <h1>The server said: <b> {myData} </b></h1>
     </div>
   );
 }
